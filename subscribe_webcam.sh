1238f6cd1590d5f8a2b118a7d4db76b7c7517bc7
Roberto Arcomano <bertolinux@gmail.com>
2021-07-02 20:11:20 +0200

    Removed a comment

--- subscribe_webcam.sh
+++ subscribe_webcam.sh
@@ -2,7 +2,6 @@
 
 SEQ=$(echo "`date +%H`*60+`date +%M`"|bc)
 TEMPDIR=/tmp/temp_webcam
-#if [ $SEQ -eq 0 ]; then
 if [ $SEQ -eq 0 ]; then
   cd $TEMPDIR
   ffmpeg -framerate 10 -pattern_type  glob -i "webcam*.jpg" /www/webcam/webcam_`date +%Y%m%d`.avi
